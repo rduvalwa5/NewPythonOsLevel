f7bf49f991ad4bbce358c1a15647dfc28aadf136
rduvalwa5 <rduvalwa5@hotmail.com>
2018-12-22 23:59:37 -0800

    add lost code

---
+++ Os_Subprocess/bashScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /Users/rduvalwa2/Desktop
+pwd
+ls -l
+echo "The quick brown fox jumped over the lazy white hound dog.This is a file to test code with." >> /Users/rduvalwa2/eOxigen-workspace/Os_level_examples/OsMisc/src/Os_Package/testFile.txt
+less /Users/rduvalwa2/eOxigen-workspace/Os_level_examples/OsMisc/src/Os_Package/testFile.txt
